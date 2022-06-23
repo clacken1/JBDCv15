6be93fc1047e2dc236b0d29005159cc72e9c3134
Haresh Kansara <hareshkansara00@gmail.com>
2022-06-23 19:13:52 +0530

    [IMP] Improved code

--- bank_reconciliation/models/account_move_line.py
+++ bank_reconciliation/models/account_move_line.py
@@ -8,16 +8,17 @@ class AccountMoveLine(models.Model):
     bank_statement_id = fields.Many2one('bank.statement', 'Bank Statement', copy=False)
     statement_date = fields.Date('Bank.St Date', copy=False)
 
-    def write(self, vals):
-        if not vals.get("statement_date"):
-            vals.update({"reconciled": False})
-            for record in self:
-                if record.payment_id and record.payment_id.state == 'reconciled':
-                    record.payment_id.state = 'posted'
-        elif vals.get("statement_date"):
-            vals.update({"reconciled": True})
-            for record in self:
-                if record.payment_id:
-                    record.payment_id.state = 'reconciled'
-        res = super(AccountMoveLine, self).write(vals)
-        return res
+    # def write(self, vals):
+    #     if not vals.get("statement_date"):
+    #         vals.update({"reconciled": False})
+    #         for record in self:
+    #             if record.payment_id and record.payment_id.state == 'reconciled':
+    #                 record.payment_id.state = 'posted'
+    #             # if record.payment_id and record.payment_id.is_reconciled and record.payment_id.state != 'posted':
+    #             #     record.payment_id.state = 'posted'
+    #     elif vals.get("statement_date"):
+    #         vals.update({"reconciled": True})
+    #         for record in self:
+    #             if record.payment_id:
+    #                 record.payment_id.state = 'reconciled'
+    #     return super(AccountMoveLine, self).write(vals)
