4cac01c1706d400dec65540a02c702037011d10c
Haresh Kansara <hareshkansara00@gmail.com>
2022-06-28 19:47:58 +0530

    [IMP] Improved code

--- bank_reconciliation/wizard/bank_statement_wiz.py
+++ bank_reconciliation/wizard/bank_statement_wiz.py
@@ -8,40 +8,47 @@ class BankStatement(models.Model):
 
     @api.onchange('journal_id', 'date_from', 'date_to', 'account_id')
     def _get_lines(self):
-        # self.account_id = self.journal_id.default_debit_account_id.id or self.journal_id.default_credit_account_id.id
-        # self.account_id = self.journal_id.default_account_id.id
-        self.currency_id = self.journal_id.currency_id or self.journal_id.company_id.currency_id or \
-            self.env.user.company_id.currency_id
-        domain = [('account_id', '=', self.account_id.id),
-                  ('statement_date', '=', False)]
-        if self.date_from:
-            domain += [('date', '>=', self.date_from)]
-        if self.date_to:
-            domain += [('date', '<=', self.date_to)]
-        s_lines = []
-        lines = self.env['account.move.line'].search(domain)
-        for line in self.statement_lines:
-            line.bank_statement_id = self.id
-        self.statement_lines = lines
+        for record in self:
+            # record.account_id = record.journal_id.default_debit_account_id.id or record.journal_id.default_credit_account_id.id
+            # record.account_id = record.journal_id.default_account_id.id
+            record.currency_id = record.journal_id.currency_id or record.journal_id.company_id.currency_id or \
+                self.env.user.company_id.currency_id
+            domain = [('account_id', '=', record.account_id.id),
+                      ('statement_date', '=', False)]
+            if record.date_from:
+                domain += [('date', '>=', record.date_from)]
+            if record.date_to:
+                domain += [('date', '<=', record.date_to)]
+            s_lines = []
+            lines = self.env['account.move.line'].search(domain)
+            for line in record.statement_lines:
+                line.bank_statement_id = record.id
+            record.statement_lines = lines
 
     @api.depends('statement_lines.statement_date')
     def _compute_amount(self):
-        gl_balance = 0
-        bank_balance = 0
-        current_update = 0
-        domain = [('account_id', '=', self.account_id.id)]
-        lines = self.env['account.move.line'].search(domain)
-        gl_balance += sum([line.debit - line.credit for line in lines])
-        domain += [('id', 'not in', self.statement_lines.ids),
-                   ('statement_date', '!=', False)]
-        lines = self.env['account.move.line'].search(domain)
-        bank_balance += sum([line.balance for line in lines])
-        current_update += sum([line.debit -
-                               line.credit if line.statement_date else 0 for line in self.statement_lines])
+        for record in self:
+            gl_balance = 0
+            bank_balance = 0
+            current_update = 0
+            domain = [('account_id', '=', record.account_id.id)]
+            lines = self.env['account.move.line'].search(domain)
+            gl_balance += sum([line.debit - line.credit for line in lines])
+            domain += [('id', 'not in', record.statement_lines.ids),
+                       ('statement_date', '!=', False)]
+            lines = self.env['account.move.line'].search(domain)
+            bank_balance += sum([line.balance for line in lines])
+            current_update += sum([line.debit -
+                                   line.credit if line.statement_date else 0 for line in record.statement_lines])
 
-        self.gl_balance = gl_balance
-        self.bank_balance = bank_balance + current_update
-        self.balance_difference = self.gl_balance - self.bank_balance
+            record.gl_balance = gl_balance
+            # record.bank_balance = bank_balance + current_update
+            record.balance_difference = record.gl_balance - (bank_balance + current_update)
+
+    @api.depends('statement_lines.statement_date')
+    def _compute_amount_bank_balance(self):
+        for record in self:
+            record._compute_amount()
 
     journal_id = fields.Many2one('account.journal', 'Bank', domain=[
                                  ('type', '=', 'bank')])
@@ -52,7 +59,7 @@ class BankStatement(models.Model):
     gl_balance = fields.Monetary(
         'Balance as per Company Books', readonly=True, compute='_compute_amount')
     bank_balance = fields.Monetary(
-        'Balance as per Bank', readonly=True, compute='_compute_amount')
+        'Balance as per Bank', readonly=False, compute='_compute_amount_bank_balance', store=True)
     balance_difference = fields.Monetary(
         'Amounts not Reflected in Bank', readonly=True, compute='_compute_amount')
     current_update = fields.Monetary('Balance of entries updated now')
